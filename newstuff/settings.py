8e9a3f0bc33a7a3ab4a62f5b1d5de4e82c751a19
Alisiddique <alisiddique10@hotmail.com>
2021-10-24 17:06:33 +0100

    gf

--- newstuff/settings.py
+++ newstuff/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'newstuff.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': BASE_DIR / 'postgres',
     }
 }
 
